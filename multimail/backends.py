0d392806008ad3dc386504216c242eb3334c4de4
Daniel Barreto <dbarreto@talpor.com>
2013-11-25 22:37:59 -0430

    PEP8 changes.

--- multimail/backends.py
+++ multimail/backends.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.backends import ModelBackend
 from multimail.models import EmailAddress
 
+
 class MultiEmailAuthenticationBackend(ModelBackend):
-    """Allows to login via email address and password. username is interpreted as email address."""
-
+    """Allows to login via email address and password. username is interpreted
+    as email address.
+    """
     def authenticate(self, username=None, password=None):
-
         try:
             email = EmailAddress.objects.get(email__iexact=username)
             user = email.user
